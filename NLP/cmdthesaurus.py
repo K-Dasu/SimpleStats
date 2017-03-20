0cebdeaf1f5990fb7fe3773ab8d78cc08c9c5f75
John Chan <jmchan100@gmail.com>
2017-03-19 17:18:25 -0700

    calculate cmd

--- NLP/cmdthesaurus.py
+++ NLP/cmdthesaurus.py
@@ -5,45 +5,36 @@ class CmdThesaurus:
 
     def __init__(self):
 
+        # verbs
         self.quitSynonyms = []
         self.openSynonyms = []
         self.showSynonyms = []
+        self.calculateSynonyms = []
         # sums - total, sum, sigma
         # calculate, get
         # while, repeat
-        synonyms = []
 
         # build quit synonyms
-        synonyms = self.buildSynList(synonyms, "exit", wn.VERB)
-        synonyms = list(set(synonyms))
-
-        synonyms = self.buildSynList(synonyms, "quit", wn.VERB)
-        synonyms = list(set(synonyms))
-
-        self.quitSynonyms = synonyms
+        self.quitSynonyms = buildSynList(self.quitSynonyms, "exit", wn.VERB)
+        self.quitSynonyms = buildSynList(self.quitSynonyms, "quit", wn.VERB)
+        self.quitSynonyms = list(set(self.quitSynonyms))
 
         #build read/open synonyms
-        synonyms = []
-
-        synonyms = self.buildSynList(synonyms, "read", wn.VERB)
-        synonyms = list(set(synonyms))
-
-        synonyms = self.buildSynList(synonyms, "open", wn.VERB)
-        synonyms = list(set(synonyms))
-
-        self.openSynonyms = synonyms
+        self.openSynonyms = buildSynList(self.openSynonyms, "read", wn.VERB)
+        self.openSynonyms = buildSynList(self.openSynonyms, "open", wn.VERB)
+        self.openSynonyms = list(set(self.openSynonyms))
 
         #build show/print synonyms
-        synonyms = []
+        self.showSynonyms = buildSynList(self.showSynonyms, "show", wn.VERB)
+        self.showSynonyms = buildSynList(self.showSynonyms, "print", wn.VERB)
+        self.showSynonyms = list(set(self.showSynonyms))
 
-        synonyms = self.buildSynList(synonyms, "show", wn.VERB)
-        synonyms = list(set(synonyms))
+        #build show/print synonyms
+        self.calculateSynonyms = buildSynList(self.calculateSynonyms, "get", wn.VERB)
+        self.calculateSynonyms = buildSynList(self.calculateSynonyms, "calculate", wn.VERB)
+        self.calculateSynonyms = list(set(self.calculateSynonyms))
 
-        synonyms = self.buildSynList(synonyms, "print", wn.VERB)
-        synonyms = list(set(synonyms))
-
-        self.showSynonyms = synonyms
-
+        
     # returns true if word is a synonym of quit
     def isQuitSynonym(self, word):
         return (word in self.quitSynonyms)
@@ -56,15 +47,19 @@ class CmdThesaurus:
     def isOpenSynonym(self, word):
         return (word in self.showSynonyms)
 
-    # adds synonyms of words to a list
-    # li - the list to add to -- will return this
-    # word - the word to find synonyms for
-    # pos - the type of word it is, i.e. noun, verb, etc. as a char, see 
-    # the wordnet pos list, see comment at top of class
-    def buildSynList(self, li, word, pos):
+    # returns true if word is a synonym of calculate
+    def isCalculateSynonym(self, word):
+        return (word in self.calculateSynonyms)
 
-        # filters out words based on pos
-        for syn in [x for x in wn.synsets(word) if x.pos() == pos]:
-            for l in syn.lemmas():
-                li.append(l.name())
-        return li
+# adds synonyms of words to a list
+# li - the list to add to -- will return this
+# word - the word to find synonyms for
+# pos - the type of word it is, i.e. noun, verb, etc. as a char, see 
+# the wordnet pos list, see comment at top of class
+def buildSynList(li, word, pos):
+
+    # filters out words based on pos
+    for syn in [x for x in wn.synsets(word) if x.pos() == pos]:
+        for l in syn.lemmas():
+            li.append(l.name())
+    return li
