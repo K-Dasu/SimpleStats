623c1ca1acbd8e546500ac4e000c673d1e2bf812
John Chan <jmchan100@gmail.com>
2017-03-21 23:27:04 -0700

    small fix to calculate

--- NLP/synthesis.py
+++ NLP/synthesis.py
@@ -275,7 +275,10 @@ class Synthesizer:
             print('something went wrong in a calculate command: no args')
             return
 
-        arg = args[0]
+        if isinstance(args, list):
+            arg = args[0]
+        else:
+            arg = args
 
         # if this is even a pair, we need to keep parsing
         if isinstance(arg, tuple):
@@ -331,7 +334,10 @@ class Synthesizer:
             print('Something went wrong in a show command: no args')
             return
 
-        arg = args[0]
+        if isinstance(args, list):
+            arg = args[0]
+        else:
+            arg = args
 
         # if this is even a pair, we need to keep parsing
         if isinstance(arg, tuple):
